9b11d9d6bfac8818050e0a34d3e23656bf076ded
Justin Schwartzenberger <Justin.Schwartzenberger@SoCreate.it>
2017-04-18 09:08:31 -0700

    update template outlet

--- src/app/examples/ng-template-outlet.component.ts
+++ src/app/examples/ng-template-outlet.component.ts
@@ -8,14 +8,15 @@ import { Component } from '@angular/core';
     </h4>
 
     <ng-container *ngTemplateOutlet="product; context: {item:item1}"></ng-container>
-    <ng-container *ngTemplateOutlet="product; context: {item:item2}"></ng-container>
+    <ng-container [ngTemplateOutlet]="product" [ngOutletContext]="{item:item2}"></ng-container>
     
     <ng-template #product let-product="item">
       <div>
         {{product.name}} {{product.price}}
       </div>
     </ng-template>
-  `
+  `,
+  styles: [`div{font-weight:bold;padding:1em;border-bottom:2px solid #000;}`]
 })
 export class NgTemplateOutletComponent {
   item1 = {name: 'Quadcopter', price: 100};
