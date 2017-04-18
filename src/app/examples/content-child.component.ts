09a287d3668655d5554c65f9f2fc3f54739d1802
Justin Schwartzenberger <schwarty@gmail.com>
2017-04-17 21:07:09 -0700

    contentchild docs link

--- src/app/examples/content-child.component.ts
+++ src/app/examples/content-child.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
   selector: 'til-content-child',
   template: `
     <h2>@ContentChild Example</h2>
+    <h4><a href="https://angular.io/docs/ts/latest/api/core/index/ContentChild-decorator.html">Documentation</a></h4>
     <form>
       <til-textbox-container label="Enter a Username">
         <input type="text" name="username" ngModel required tilTextbox>
