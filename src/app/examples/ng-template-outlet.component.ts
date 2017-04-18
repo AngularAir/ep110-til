0f48585f8666caf86a7e3b07bee07ccd08f734cd
Justin Schwartzenberger <schwarty@gmail.com>
2017-04-17 21:22:57 -0700

    ngtemplateoutlet example

--- src/app/examples/ng-template-outlet.component.ts
+++ src/app/examples/ng-template-outlet.component.ts
@@ -1,6 +1,23 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'til-ng-template-outlet',
-  template: `<h2>NgTemplateOutlet Example</h2>`
+  template: `
+    <h2>NgTemplateOutlet Example</h2>
+    <h4>
+      <a href="https://angular.io/docs/ts/latest/api/common/index/NgTemplateOutlet-directive.html">Documentation</a>
+    </h4>
+
+    <ng-container *ngTemplateOutlet="product; context: {item:item1}"></ng-container>
+    <ng-container *ngTemplateOutlet="product; context: {item:item2}"></ng-container>
+    
+    <ng-template #product let-product="item">
+      <div>
+        {{product.name}} {{product.price}}
+      </div>
+    </ng-template>
+  `
 })
-export class NgTemplateOutletComponent {}
+export class NgTemplateOutletComponent {
+  item1 = {name: 'Quadcopter', price: 100};
+  item2 = {name: 'Wooden train', price: 5};
+}
