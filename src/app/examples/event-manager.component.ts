547252e03442a6041b3e23e57b870eba0235a782
Justin Schwartzenberger <Justin.Schwartzenberger@SoCreate.it>
2017-04-18 08:38:41 -0700

    tweak to key binding

--- src/app/examples/event-manager.component.ts
+++ src/app/examples/event-manager.component.ts
@@ -4,22 +4,37 @@ import { EventManager } from '@angular/platform-browser';
   selector: 'til-event-manager',
   template: `
     <h2>EventManager Example</h2>
-    <h4><a href="https://angular.io/docs/ts/latest/api/platform-browser/index/EventManager-class.html">Documentation</a></h4>
+    <h4><a href="https://angular.io/docs/ts/latest/api/platform-browser/index/EventManager-class.html">Documentation</a>
+    </h4>
     <div [class.selected]="selected">Some Item</div>
     <span>Hit ctrl+a to toggle select</span>
   `,
   styles: [`
-    div {padding:2em;border:2px solid #666;width: 5em;}
-    .selected {background-color:#999;border:2px solid #333;}
-    span {display:inline-block;margin-top:2em;font-style:italic;}
+    div {
+      padding: 2em;
+      border: 2px solid #666;
+      width: 5em;
+    }
+
+    .selected {
+      background-color: #999;
+      border: 2px solid #333;
+    }
+
+    span {
+      display: inline-block;
+      margin-top: 2em;
+      font-style: italic;
+    }
   `]
 })
 export class EventManagerComponent {
   selected = false;
+
   constructor(private eventManager: EventManager) {
     this.eventManager.addGlobalEventListener(
       'window',
-      'keydown.control.a',
+      'keyup.a',
       () => {
         this.selected = !this.selected;
       });
